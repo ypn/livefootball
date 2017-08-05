19b1bf1d95a3329ed4836e24187890156c689ca8
Pham Nhu Y <ypnwebdev@gmail.com>
2017-08-05 17:31:58 +0700

    len

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,7 +44,28 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+          if($this->isHttpException($exception))
+           {
+               switch ($exception->getStatusCode()) {
+                   // not found
+                   case 404:
+                       return \Response::view('errors.404',array(),404);
+                   break;
+                   // internal error
+                   case 500:
+                       return \Response::view('errors.500',array(),500);
+                   break;
+
+                   default:
+                       return $this->renderHttpException($e);
+                   break;
+               }
+           }
+           else
+           {
+               return parent::render($request, $exception);
+         }
+
     }
 
     /**
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="content-style-type" content="text/css" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>CSS Ninjas &middot; Shinobi Not Found (404)</title>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="http://www.cssninjas.com/styles/screen.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="http://www.cssninjas.com/styles/print.css" media="print" />
+	<!--[if lt IE 7]>
+		<link rel="stylesheet" media="screen" href="/styles/ielt7.css" type="text/css"/>
+	<![endif]-->
+	<script type="text/javascript" src="http://www.cssninjas.com/javascript/scripts.js"></script>
+</head>
+<body id="e404">
+
+<div id="root">
+	<div id="content">
+		<div class="outer">
+			<div id="error">
+				<h1 class="a">Shinobi.. 404!</h1>
+				<p>The page you were looking for appears to have been moved, deleted or does not exist. You could go back to <span class="go-back">where you were</span> or head straight to our <a href="/">home page</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
