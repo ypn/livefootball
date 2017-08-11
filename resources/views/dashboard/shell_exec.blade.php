30b90f504b7118efa84eb8747d495d2d2b974a78
Pham Nhu Y <ypnwebdev@gmail.com>
2017-08-11 09:29:41 +0700

    ok

--- resources/views/dashboard/shell_exec.blade.php
+++ resources/views/dashboard/shell_exec.blade.php
@@ -39,7 +39,7 @@ document.getElementById('liveFacebook').onclick = function() {
         _token:"<?php echo(csrf_token()); ?>",
         file_url: $('textarea[name="file_path"]').val(),
         stream_url:response.stream_url
-      }     
+      },     
       success:function(){
         FB.ui({
           display: 'popup',
