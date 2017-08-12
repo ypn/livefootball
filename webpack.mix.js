cf11964ae9070817dd78fe0cf6e6763ed7aaecd6
Pham Nhu Y <ypnwebdev@gmail.com>
2017-08-12 19:02:08 +0700

    ok

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,8 @@ const { mix } = require('laravel-mix');
 
 mix//.js('resources/assets/js/app.js', 'public/js')
   //.sass('resources/assets/sass/app.scss', 'public/css')
-  .sass('resources/assets/sass/master.scss', 'public/css')
-  .react('resources/assets/js/components/module_chat/chat-master.js','public/js/plugins')
+  //.sass('resources/assets/sass/master.scss', 'public/css')
+  //.react('resources/assets/js/components/module_chat/chat-master.js','public/js/plugins')
   //  .browserSync('http://localhost/livefootball/public/')
   //  .disableNotifications();
   // .styles([
@@ -23,14 +23,14 @@ mix//.js('resources/assets/js/app.js', 'public/js')
   //   'resources/assets/lib/videojs/custom.css',
   //   // 'resources/assets/lib/videojs/videojs-dvrseekbar.css'
   // ],'public/js/lib/ainokishi.css')
-  // .scripts([
-  //   'resources/assets/lib/videojs/stackblur.min.js',
-  //   'resources/assets/lib/videojs/video.js',
-  //   'resources/assets/lib/videojs/videojs-ie8.min.js',
-  //   'resources/assets/lib/videojs/videojs-flash.js',
-  //   'resources/assets/lib/videojs/videojs-contrib-hls.js',
-  //   'resources/assets/lib/videojs/videojs-errors.js',
-  //   //'resources/assets/lib/videojs/videojs-dvrseekbar.min.js',
-  //   'resources/assets/lib/videojs/config.js'
-  // ],'public/js/lib/ainokishi.js')
+  .scripts([
+    'resources/assets/lib/videojs/stackblur.min.js',
+    'resources/assets/lib/videojs/video.js',
+    'resources/assets/lib/videojs/videojs-ie8.min.js',
+    'resources/assets/lib/videojs/videojs-flash.js',
+    'resources/assets/lib/videojs/videojs-contrib-hls.js',
+    'resources/assets/lib/videojs/videojs-errors.js',
+    //'resources/assets/lib/videojs/videojs-dvrseekbar.min.js',
+    'resources/assets/lib/videojs/config.js'
+  ],'public/js/lib/ainokishi.js')
   ;
